3f9965dd9f868f807d492cff8d66e8a438b8991a
Ewa Kubik <ewa.kubik@hotmail.com>
2022-04-14 01:21:59 +1000

    get all/single users

---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  getAllUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
