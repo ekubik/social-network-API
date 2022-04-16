518f44ebff585743ff3677553b025b32b43b80fd
Ewa Kubik <ewa.kubik@hotmail.com>
2022-04-16 23:00:32 +1000

    fix error

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const thoughtRoutes = require("./thoughtsRoutes");
 const userRoutes = require("./userRoutes");
 
 router.use("/thoughts", thoughtRoutes);
-router.user("/users", userRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
